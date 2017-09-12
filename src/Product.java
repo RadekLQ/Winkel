b8292b07a6ac48c77a726ed16b86b10d241614b3
Maya <mayavc@icloud.com>
2017-09-12 14:49:26 +0200

    Klasse Product

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,26 @@
 public class Product {
 
+    private String omschrijving;
+    private double prijs;
+
+    public Product(String omschrijving, double prijs) {
+        this.omschrijving = omschrijving;
+        this.prijs = prijs;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public void setOmschrijving(String omschrijving) {
+        this.omschrijving = omschrijving;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(double prijs) {
+        this.prijs = prijs;
+    }
 }
